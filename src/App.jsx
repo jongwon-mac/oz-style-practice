37bd3df4627fc1358bce94110dac94dc7adf1a93
jongwon-mac <jaketd15@gmail.com>
2025-07-14 02:28:59 -0700

    11일차과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import styled from "styled-components";
+
+const Section = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  padding: 20px 40px;
+`;
 
 function App() {
   return (
     <main>
       <Header />
-      <section>
+      <Section>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </Section>
     </main>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,33 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: black;
+`;
+
+const MenuList = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  list-style: none;
+  font-weight: 400;
+  font-size: 13px;
+  color: white;
+`;
+
 export default function Header() {
   return (
-    <header className="header-container">
-      <h1>OZ코딩스쿨</h1>
-      <ul className="menu-container">
+    <HeaderContainer>
+      <h1 style={{ color: "white" }}>OZ코딩스쿨</h1>
+      <MenuList>
         <li>로그인</li>
         <li>회원가입</li>
         <li>내클래스</li>
-      </ul>
-    </header>
+      </MenuList>
+    </HeaderContainer>
   );
 }
